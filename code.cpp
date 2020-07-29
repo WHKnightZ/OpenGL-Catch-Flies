2b0bba3dfcd19fb38cff58ab81671363808f1eed
Nguyễn Khánh <48227422+WHKnightZ@users.noreply.github.com>
2020-07-29 10:28:02 +0700

    Update code.cpp
    clone or download thư viện trước
    sau đó tạo project
    cài đặt
    cop anh?
    code 1 file, về sau tách ra
    cuối cùng tham khảo code mình up trên github

--- code.cpp
+++ code.cpp
@@ -4,8 +4,8 @@
 #include "../Library/loadpng.h"
 #include "../Library/process_image.h"
 #include "../Library/gl_texture.h"
-#define WIDTH 880
-#define HEIGHT 464
+#define WIDTH 720
+#define HEIGHT 384
 int main(int argc, char **argv) {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
@@ -47,19 +47,7 @@ void Timer(int value) {
 }
 	glutTimerFunc(0, Timer, 0);
 	printf("Hello");
-#define START_X 80
-#define START_Y 80
-Rect Rct_Left = {0, START_X - 6.0f, START_Y, HEIGHT},
-	 Rct_White_Left = {START_X - 6.0f, START_X, START_Y, HEIGHT},
-	 Rct_Right = {WIDTH - START_X + 6.0f, WIDTH, START_Y, HEIGHT},
-	 Rct_White_Right = {WIDTH - START_X, WIDTH - START_X + 6.0f, START_Y, HEIGHT},
-	 Rct_Bottom = {0, WIDTH, 0, START_Y - 6.0f},
-	 Rct_White_Bottom = {0, WIDTH, START_Y - 6.0f, START_Y};
-Rect Rct_Background = {START_X, 720 + START_X, 48 + START_Y, 384 + START_Y},
-	 Rct_Ground = {START_X, 720 + START_X, START_Y, 48 + START_Y};
-float Color_Left_Right[][3] = {{0.600f, 0.819f, 0.945f}, {0.345f, 0.561f, 0.698f}},
-	 Color_Bottom[] = {0.267f, 0.267f, 0.267f};
-float Color_White[] = {1.0f, 1.0f, 1.0f};
+Rect Rct_Background = {0, 720, 48, 384}, Rct_Ground = {0, 720, 0, 48};
 Image Img_Background, Img_Ground;
 #define SCALE 3
 void Init_Game() {
@@ -73,43 +61,6 @@ void Init_Game() {
 	Draw_Rect(&Rct_Background);
 	Map_Texture(&Img_Ground);
 	Draw_Rect(&Rct_Ground);
-void Draw_Frame() {
-	glDisable(GL_TEXTURE_2D);
-	glBegin(GL_QUADS);
-	glColor3fv(Color_Left_Right[0]);
-	glVertex2f(Rct_Left.Left, Rct_Left.Bottom);
-	glVertex2f(Rct_Left.Right, Rct_Left.Bottom);
-	glColor3fv(Color_Left_Right[1]);
-	glVertex2f(Rct_Left.Right, Rct_Left.Top);
-	glVertex2f(Rct_Left.Left, Rct_Left.Top);
-	glColor3fv(Color_Left_Right[0]);
-	glVertex2f(Rct_Right.Left, Rct_Right.Bottom);
-	glVertex2f(Rct_Right.Right, Rct_Right.Bottom);
-	glColor3fv(Color_Left_Right[1]);
-	glVertex2f(Rct_Right.Right, Rct_Right.Top);
-	glVertex2f(Rct_Right.Left, Rct_Right.Top);
-	glColor3fv(Color_Bottom);
-	glVertex2f(Rct_Bottom.Left, Rct_Bottom.Bottom);
-	glVertex2f(Rct_Bottom.Right, Rct_Bottom.Bottom);
-	glVertex2f(Rct_Bottom.Right, Rct_Bottom.Top);
-	glVertex2f(Rct_Bottom.Left, Rct_Bottom.Top);
-	glColor3fv(Color_White);
-	glVertex2f(Rct_White_Left.Left, Rct_White_Left.Bottom);
-	glVertex2f(Rct_White_Left.Right, Rct_White_Left.Bottom);
-	glVertex2f(Rct_White_Left.Right, Rct_White_Left.Top);
-	glVertex2f(Rct_White_Left.Left, Rct_White_Left.Top);
-	glVertex2f(Rct_White_Right.Left, Rct_White_Right.Bottom);
-	glVertex2f(Rct_White_Right.Right, Rct_White_Right.Bottom);
-	glVertex2f(Rct_White_Right.Right, Rct_White_Right.Top);
-	glVertex2f(Rct_White_Right.Left, Rct_White_Right.Top);
-	glVertex2f(Rct_White_Bottom.Left, Rct_White_Bottom.Bottom);
-	glVertex2f(Rct_White_Bottom.Right, Rct_White_Bottom.Bottom);
-	glVertex2f(Rct_White_Bottom.Right, Rct_White_Bottom.Top);
-	glVertex2f(Rct_White_Bottom.Left, Rct_White_Bottom.Top);
-	glEnd();
-	glEnable(GL_TEXTURE_2D);
-}
-	Draw_Frame();
 #define CELL_SIZE 24
 #define MAX_X 30
 #define MAX_Y 24
@@ -135,8 +86,8 @@ Image c_Platformer::Img_Save;
 	void Init(int _x, int _y) {
 		Map[_y][_x] = Map[_y][_x + 1] = Map[_y][_x + 2] = Map[_y][_x + 3] = 1;
 		Img = &Img_Save;
-		float x = START_X + (_x + 2) * CELL_SIZE;
-		float y = START_Y + _y * CELL_SIZE;
+		float x = (_x + 2) * CELL_SIZE;
+		float y = _y * CELL_SIZE;
 		Rct.Left = x - Img->w / 2;
 		Rct.Right = Rct.Left + Img->w;
 		Rct.Bottom = y;
@@ -171,8 +122,8 @@ Image c_Cloud::Img_Save;
 	float x, y;
 	void Init(float _x, float _y) {
 		Img = &Img_Save;
-		x = _x + START_X;
-		y = _y + START_Y;
+		x = _x;
+		y = _y;
 		Update_Rect();
 		Rct.Bottom = y;
 		Rct.Top = Rct.Bottom + Img->h;
@@ -203,11 +154,10 @@ c_Cloud Clouds[CLOUD_COUNT];
 class c_Frog {
   public:
 	static Image Img_Save[2][2][2];
-	static Image Img_Button_Save[2], Img_Button_Shadow;
 	static float Map_Offset[2];
 	static float Map_Base_Angle[2];
-	Rect Rct, Rct_Button, Rct_Button_Shadow, Rct_Score;
-	Image *Img, *Img_Button, *Img_Score;
+	Rect Rct;
+	Image *Img;
 	float x, y, vx, vy;
 	int Player;
 	int Drt, Anim;
@@ -223,7 +173,7 @@ class c_Frog {
 		Anim = 0;
 		float Offset = 11.0f * CELL_SIZE * (Player == 0 ? -1 : 1);
 		x = WIDTH / 2 + Offset;
-		y = CELL_SIZE * 2.0f + START_Y;
+		y = CELL_SIZE * 2.0f;
 		Prepare_Stt = 0;
 		Is_Jumping = false;
 		Is_Jump_Pressed = false;
@@ -261,19 +211,9 @@ class c_Frog {
 		Flip_Horizontal(&Img_Save[1][1][0], &Img_Save[1][0][0]);
 		Flip_Horizontal(&Img_Save[1][1][1], &Img_Save[1][0][1]);
 		Delete_Image(&Img);
-		Load_Texture(&Img, "Images/Buttons.png");
-		Crop_Image(&Img, &Img_Button_Save[0], 0, 0, 44, 24);
-		Crop_Image(&Img, &Img_Button_Save[1], 0, 24, 44, 24);
-		Swap_Image(Img_Button_Save[0].img, 44, 24);
-		Swap_Image(Img_Button_Save[1].img, 44, 24);
-		Zoom_Image(&Img_Button_Save[0], SCALE);
-		Zoom_Image(&Img_Button_Save[1], SCALE);
-		unsigned char Color_Shadow[] = {0, 0, 0, 50};
-		Clone_Image_Shadow(&Img_Button_Save[0], &Img_Button_Shadow, Color_Shadow);
-		Delete_Image(&Img);
 	}
 };
-Image c_Frog::Img_Save[2][2][2], c_Frog::Img_Button_Save[2], c_Frog::Img_Button_Shadow;
+Image c_Frog::Img_Save[2][2][2];
 float c_Frog::Map_Offset[2] = {-1.0f, 1.0f};
 float c_Frog::Map_Base_Angle[2] = {160.0f, 20.0f};
 c_Frog Frogs[2];
@@ -319,14 +259,14 @@ printf("Up");
 			if (vy < -24.0f)
 				vy = -24.0f;
 			if (vy <= 0.0f) {
-				int col1 = (x - START_X - 9.0f) / CELL_SIZE;
-				int col2 = (x - START_X + 9.0f) / CELL_SIZE;
-				int row_old = (y_old - START_Y) / CELL_SIZE;
-				int row = (y - START_Y) / CELL_SIZE;
+				int col1 = (x - 9.0f) / CELL_SIZE;
+				int col2 = (x + 9.0f) / CELL_SIZE;
+				int row_old = y_old / CELL_SIZE;
+				int row = y / CELL_SIZE;
 				if ((!Map[row_old][col1] && !Map[row_old][col2]) &&
 					(Map[row][col1] || Map[row][col2])) {
 					Is_Jumping = false;
-					y = (row + 1) * CELL_SIZE + START_Y;
+					y = (row + 1) * CELL_SIZE;
 					vx = 0.0f;
 					vy = 0.0f;
 					Anim = 0;
@@ -350,7 +290,7 @@ printf("Up");
 		vx = 7.0f;
 		vy = 7.0f;
 		Jump();
-float BOUNDARY_LEFT = START_X + 30.0f, BOUNDARY_RIGHT = START_X + 690.0f;
+float BOUNDARY_LEFT = 30.0f, BOUNDARY_RIGHT = 690.0f;
 	static bool Check_Boundary_Left(float x) { return x < BOUNDARY_LEFT; }
 	static bool Check_Boundary_Right(float x) { return x > BOUNDARY_RIGHT; }
 	static bool (*Check_Boundary[2])(float x);
@@ -485,8 +425,8 @@ class c_Fly {
 	int Timer, Anim, Region;
 	bool Is_Alive;
 	c_Fly(float _x, float _y, int _Region) {
-		x = _x + rand() % 41 - 20 + START_X;
-		y = _y + rand() % 41 - 20 + START_Y;
+		x = _x + rand() % 41 - 20;
+		y = _y + rand() % 41 - 20;
 		vx = vx_Max;
 		vy = 0.0f;
 		ax = ax_Base;
